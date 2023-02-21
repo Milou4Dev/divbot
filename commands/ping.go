c37ca83390114299a07c66946430c5742eccb4be
Émile Fortier <108042484+Milou4Dev@users.noreply.github.com>
2023-02-21 16:05:00 -0600

    Update ping.go

--- commands/ping.go
+++ commands/ping.go
@@ -2,9 +2,12 @@ package commands
 
 import (
     "time"
+    "fmt"
 
     "github.com/bwmarrin/discordgo"
 )
+
+const pingCooldown = 5 * time.Second
 
 var (
     pingCooldowns = make(map[string]time.Time)
@@ -13,9 +16,10 @@ var (
 func PingCommand(s *discordgo.Session, m *discordgo.MessageCreate) {
     // Vérifier si l'utilisateur a déjà utilisé la commande "ping" dans les 5 dernières secondes.
     if lastUsed, ok := pingCooldowns[m.Author.ID]; ok {
-        if time.Now().Sub(lastUsed) < 5*time.Second {
+        if time.Since(lastUsed) < pingCooldown {
             // L'utilisateur a utilisé la commande "ping" trop récemment. Envoyer un message d'erreur.
-            s.ChannelMessageSend(m.ChannelID, "Vous devez attendre encore "+(5*time.Second-time.Now().Sub(lastUsed)).String()+" avant d'utiliser la commande de nouveau.")
+            remainingCooldown := pingCooldown - time.Since(lastUsed)
+            s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Vous devez attendre encore %s avant d'utiliser la commande de nouveau.", remainingCooldown))
             return
         }
     }
