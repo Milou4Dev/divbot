ea794c04d9b52b73d7d873b89589437e8b6ece9c
Émile Fortier <108042484+Milou4Dev@users.noreply.github.com>
2023-02-21 19:00:46 -0600

    Update ping.go

--- commands/ping.go
+++ commands/ping.go
@@ -1,47 +1,55 @@
 package commands
 
 import (
-    "time"
-    "fmt"
+	"fmt"
+	"time"
 
-    "github.com/bwmarrin/discordgo"
+	"github.com/bwmarrin/discordgo"
 )
 
 const pingCooldown = 5 * time.Second
 
 var (
-    pingCooldowns = make(map[string]time.Time)
+	pingCooldowns = make(map[string]time.Time)
 )
 
 func PingCommand(s *discordgo.Session, m *discordgo.MessageCreate) {
-    // Vérifier si l'utilisateur a déjà utilisé la commande "ping" dans les 5 dernières secondes.
-    if lastUsed, ok := pingCooldowns[m.Author.ID]; ok {
-        if time.Since(lastUsed) < pingCooldown {
-            // L'utilisateur a utilisé la commande "ping" trop récemment. Envoyer un message d'erreur.
-            remainingCooldown := pingCooldown - time.Since(lastUsed)
-            s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Vous devez attendre encore %s avant d'utiliser la commande de nouveau.", remainingCooldown))
-            return
-        }
-    }
+	// Vérifier si l'utilisateur a déjà utilisé la commande "ping" dans les 5 dernières secondes.
+	if lastUsed, ok := pingCooldowns[m.Author.ID]; ok {
+		if time.Since(lastUsed) < pingCooldown {
+			// L'utilisateur a utilisé la commande "ping" trop récemment. Envoyer un message d'erreur.
+			remainingCooldown := pingCooldown - time.Since(lastUsed)
+			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Vous devez attendre encore %s avant d'utiliser la commande de nouveau.", remainingCooldown))
+			return
+		}
+	}
 
-    // Enregistre l'heure actuelle comme dernière utilisation de la commande "ping".
-    pingCooldowns[m.Author.ID] = time.Now()
+	// Enregistre l'heure actuelle comme dernière utilisation de la commande "ping".
+	pingCooldowns[m.Author.ID] = time.Now()
 
-    // Enregistre l'heure actuelle
-    start := time.Now()
+	// Enregistre l'heure actuelle
+	start := time.Now()
 
-    // Envoie un message "ping"
-    msg, err := s.ChannelMessageSend(m.ChannelID, "ping")
-    if err != nil {
-        // En cas d'erreur, envoie un message d'erreur
-        s.ChannelMessageSend(m.ChannelID, "Une erreur est survenue lors de l'envoi du message.")
-        return
-    }
+	// Obtient le ping de l'API Discord
+	apiPing := s.HeartbeatLatency().Round(time.Millisecond)
 
-    // Calcule la durée entre l'envoi du message et sa réception
-    end := time.Now()
-    duration := end.Sub(start)
+	// Envoie un message "ping"
+	msg, err := s.ChannelMessageSend(m.ChannelID, "ping")
+	if err != nil {
+		// En cas d'erreur, envoie un message d'erreur
+		s.ChannelMessageSend(m.ChannelID, "Une erreur est survenue lors de l'envoi du message.")
+		return
+	}
 
-    // Met à jour le message avec la durée
-    s.ChannelMessageEdit(m.ChannelID, msg.ID, "Pong! Temps de réponse : "+duration.String())
+	// Calcule la durée entre l'envoi du message et sa réception
+	end := time.Now()
+	duration := end.Sub(start).Round(time.Millisecond)
+
+	// Met à jour le message avec la durée et les pings
+	embed := &discordgo.MessageEmbed{
+		Title:       "Pong!",
+		Description: fmt.Sprintf("Temps de réponse : %s\nPing de l'API : %s\nPing du bot : %s", duration, apiPing, s.HeartbeatLatency()),
+		Color:       0, // vert
+	}
+	s.ChannelMessageEditEmbed(m.ChannelID, msg.ID, embed)
 }
