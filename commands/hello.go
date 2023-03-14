bf5b31003fcb9026c264e44e30ceac156c95952b
Milou <108042484+Milou4Dev@users.noreply.github.com>
2023-03-14 14:18:39 -0400

    Update hello.go

--- commands/hello.go
+++ commands/hello.go
@@ -1,28 +1,41 @@
 package commands
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
+const helloCooldown = 5 * time.Second
+
 var (
 	helloCooldowns = make(map[string]time.Time)
 )
 
-func HelloCommand(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// Vérifier si l'utilisateur a déjà utilisé la commande "hello" dans les 5 dernières secondes.
-	if lastUsed, ok := helloCooldowns[m.Author.ID]; ok {
-		if time.Since(lastUsed) < 5*time.Second {
-			// L'utilisateur a utilisé la commande "hello" trop récemment. Envoyer un message d'erreur.
-			s.ChannelMessageSend(m.ChannelID, "Vous devez attendre encore "+(5*time.Second-time.Since(lastUsed)).String()+" avant d'utiliser la commande de nouveau.")
+func HelloCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	userID := i.Member.User.ID
+
+	if lastUsed, ok := helloCooldowns[userID]; ok {
+		if time.Since(lastUsed) < helloCooldown {
+			remainingCooldown := helloCooldown - time.Since(lastUsed)
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseTypeChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: fmt.Sprintf("You need to wait %s before using this command again.", remainingCooldown),
+					Flags:   64, // Set the message to be ephemeral (only visible to the user)
+				},
+			})
 			return
 		}
 	}
 
-	// Enregistre l'heure actuelle comme dernière utilisation de la commande "hello".
-	helloCooldowns[m.Author.ID] = time.Now()
+	helloCooldowns[userID] = time.Now()
 
-	// Envoie un message "hello"
-	s.ChannelMessageSend(m.ChannelID, "Bonjour !")
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseTypeChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: "Hello!",
+		},
+	})
 }
